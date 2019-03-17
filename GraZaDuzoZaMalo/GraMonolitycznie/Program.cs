b9665b442e890ec6b2a49e0d0b34f6f3759e6467
piotrbalon <balonpiotrek@gmail.com>
2019-03-17 12:16:03 +0100

    first version

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            Console.Write("Input name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name} !!!!");
+        }
+    }
+}
